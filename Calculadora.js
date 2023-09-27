9d4339f955a4d06efd1b5de19546457b1678121c
GonzalaLobo <g.lobonicolas@hotmail.com>
2023-09-26 21:38:44 -0300

    ss

---
+++ Calculadora.js
@@ -0,0 +1,18 @@
+// se puso en mayuscula por que va ser una clase. y por convencion los files que tienen clases se lo inician con mayuscula
+class Calculadora {
+    sumar(num1, num2) {
+        return num1 + num2;
+    }
+
+    restar(num1, num2) {
+        return num1 - num2;
+    }
+
+    dividir(num1, num2) {
+        return num1 / num2;
+    }
+
+    multiplicar(num1, num2) {
+        return num1 * num2;
+    }
+} 
